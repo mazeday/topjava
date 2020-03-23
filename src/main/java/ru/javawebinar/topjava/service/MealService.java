91afdd4ef4d18f7537df71fb8c34e59225b74786
mazeday <mazeday54@gmail.com>
2020-03-23 20:44:51 +0700

    HW2

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,13 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+@Service
 public class MealService {
 
+    @Autowired
     private MealRepository repository;
 
 }
